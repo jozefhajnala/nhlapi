a7d27a5e4d91929c1236e0bf2c3535120f4877ab
Jozef Hajnala <jozef.hajnala@gmail.com>
2020-03-18 13:32:15 +0100

    Improve test stability

--- tests/testthat/test.nhl_teams.R
+++ tests/testthat/test.nhl_teams.R
@@ -4,8 +4,8 @@ testthat::test_that(
   "nhl_teams retrieves 2 teams", {
     testthat::skip_if_offline(host = "nhl.com")
     testthat::expect_equal(
-      nhl_teams(teamIds = 1:2),
-      teams_processed
+      nhl_teams(teamIds = 1:2)[, c("id", "name", "link")],
+      teams_processed[, c("id", "name", "link")]
     )
   }
 )
