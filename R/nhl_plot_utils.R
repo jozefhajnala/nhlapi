aa39bf9f22107341dd9bddd9d5f3fa3351a7e5ff
Michael Chirico <MichaelChirico4@gmail.com>
2020-09-04 17:36:37 -0400

    simplify (hopefully) by using symmetry to plot by quadrant

--- R/nhl_plot_utils.R
+++ R/nhl_plot_utils.R
@@ -6,93 +6,91 @@
 #' @importFrom graphics lines polygon
 plot_rink = function() {
   # blank plot with correct dimensions and aspect ratio
-  plot(NA, xlim=c(-1, 1)*.5*rink_width, ylim=c(-1, 1)*.5*rink_height,
+  plot(NA, xlim=c(-1, 1)*rink_width, ylim=c(-1, 1)*rink_height,
        asp=1, bty='n', axes=FALSE, xlab='', ylab='')
-  # add straight boards
-  plot_symm_hseg(board_curve_x, rink_height/2)
-  plot_symm_hseg(board_curve_x, -rink_height/2)
-  plot_symm_vseg(-rink_width/2, board_curve_y)
-  plot_symm_vseg(rink_width/2, board_curve_y)
+  # rink is symmetric by quadrant -- write code to plot one quadrant,
+  #   and then reflect it 4 times
+  lapply(list(c(1, 1), c(1, -1), c(-1, -1), c(-1, 1)), plot_quadrant)
 
-  # add arced boards
-  plot_circle_arc(-board_curve_x, board_curve_y, board_radius, pi/2, pi)
-  plot_circle_arc(board_curve_x, board_curve_y, board_radius, 0, pi/2)
-  plot_circle_arc(board_curve_x, -board_curve_y, board_radius, -pi/2, 0)
-  plot_circle_arc(-board_curve_x, -board_curve_y, board_radius, -pi/2, -pi)
+}
+
+plot_quadrant = function(multipliers) {
+  xmult = multipliers[1L]; ymult = multipliers[2L]
+  quadrant = get_quadrant(multipliers)
+  is_even_quad = quadrant %% 2 == 0
+  theta0 = quadrant * pi/2
+  theta1 = theta0 + pi/2
+  # for the goal crease, which is not a 1/4 circle
+  alpha0 = if (xmult == 1) pi else 0
+  alpha1 = alpha0 + (if (is_even_quad) 1 else -1) * crease_theta
+
+  # add boards (circle for arced portion)
+  #   nice about segments -- defaults to being horizontal in one direction
+  segments(0, ymult*rink_height, xmult*board_curve_x)
+  segments(xmult*rink_width, ymult*board_curve_y, y1=0)
+  plot_circle_arc(xmult*board_curve_x, ymult*board_curve_y, board_radius, theta0, theta1)
 
   # add center, goal & zone lines
-  plot_symm_vseg(0, rink_height/2, col=nhl_red)
-  plot_symm_vseg(-goal_x_coord, goal_y_coord, col=nhl_red)
-  plot_symm_vseg(goal_x_coord,  goal_y_coord, col=nhl_red)
-  plot_symm_vseg(-nzone_width/2, rink_height/2, col=nhl_blue)
-  plot_symm_vseg(nzone_width/2,  rink_height/2, col=nhl_blue)
+  if (is_even_quad) segments(0, 0, y1=ymult*rink_height, col=nhl_red)
+  segments(xmult*goal_x_coord, 0, y1=ymult*goal_y_coord, col=nhl_red)
+  segments(xmult*nzone_width, 0, y1=ymult*rink_height, col=nhl_blue)
 
   # add faceoff circles
-  plot_circle(0, 0, faceoff_radius, col=nhl_blue)
-  plot_circle(-dzone_dot_x, dot_y,  faceoff_radius, col=nhl_red)
-  plot_circle(dzone_dot_x, dot_y,   faceoff_radius, col=nhl_red)
-  plot_circle(dzone_dot_x, -dot_y,  faceoff_radius, col=nhl_red)
-  plot_circle(-dzone_dot_x, -dot_y, faceoff_radius, col=nhl_red)
+  plot_circle_arc(0, 0, faceoff_radius, theta0, theta1, col=nhl_blue)
+  plot_circle(xmult*dzone_dot_x, ymult*dot_y, faceoff_radius, col=nhl_red)
 
   # add faceoff dots
-  ## within circles
-  plot_filled_circle(0, 0, .5, col=nhl_blue)
-  plot_filled_circle(-dzone_dot_x, dot_y,  dot_radius, col=nhl_red)
-  plot_filled_circle(dzone_dot_x, dot_y,   dot_radius, col=nhl_red)
-  plot_filled_circle(dzone_dot_x, -dot_y,  dot_radius, col=nhl_red)
-  plot_filled_circle(-dzone_dot_x, -dot_y, dot_radius, col=nhl_red)
-
-  ## neutral zone
-  plot_filled_circle(-dot_displacement, dot_y,  dot_radius, col=nhl_red)
-  plot_filled_circle(dot_displacement, dot_y,   dot_radius, col=nhl_red)
-  plot_filled_circle(dot_displacement, -dot_y,  dot_radius, col=nhl_red)
-  plot_filled_circle(-dot_displacement, -dot_y, dot_radius, col=nhl_red)
+  plot_sector(0, 0, center_dot_radius, theta0, theta1, col=nhl_blue)
+  plot_filled_circle(xmult*dzone_dot_x, ymult*dot_y,  dot_radius, col=nhl_red)
+  plot_filled_circle(xmult*dot_displacement, ymult*dot_y,  dot_radius, col=nhl_red)
 
   # add goal creases
   ## straight portion orthogonal to goal line
-  lines(c(-goal_x_coord, crease_x_coord), c(crease_width/2, crease_width/2),  col=nhl_red)
-  lines(c(-goal_x_coord, crease_x_coord), -c(crease_width/2, crease_width/2), col=nhl_red)
-  lines(c(goal_x_coord, -crease_x_coord), c(crease_width/2, crease_width/2),  col=nhl_red)
-  lines(c(goal_x_coord, -crease_x_coord), -c(crease_width/2, crease_width/2), col=nhl_red)
-
+  segments(xmult*goal_x_coord, ymult*crease_width, xmult*crease_x_coord, col=nhl_red)
   ## arced portion truncated at intersection with straight portion
-  plot_circle_arc(-goal_x_coord, 0, crease_radius, -crease_theta, crease_theta, col=nhl_red)
-  plot_circle_arc(goal_x_coord, 0, crease_radius, pi-crease_theta, pi+crease_theta, col=nhl_red)
+  plot_circle_arc(xmult*goal_x_coord, 0, crease_radius, alpha0, alpha1, col=nhl_red)
 }
 
 # utils/constants
 ## dimensional constants
-rink_width = 200
-rink_height = 85
+rink_width = 200/2
+rink_height = 85/2
 end_width = 11 # between goal line & end boards
-nzone_width = 50 # width of neutral zone
+nzone_width = 50/2 # width of neutral zone
 dot_displacement = 20 # between faceoff dot & reference line (goal line for D zone, center for N zone)
-dot_span = 44 # span between centers of faceoff dots
-crease_width = 8
+dot_y = 44/2 # span between centers of faceoff dots
+crease_width = 8/2
 board_radius = 28
 faceoff_radius = 15
 crease_radius = 6
 dot_radius = 1
 center_dot_radius = .5
 ### where the curved portion of the boards begins
-board_curve_x = rink_width/2 - board_radius
-board_curve_y = rink_height/2 - board_radius
+board_curve_x = rink_width - board_radius
+board_curve_y = rink_height - board_radius
 ### where the goal line intersects the curved portion of the boards
-goal_x_coord = rink_width/2 - end_width
+goal_x_coord = rink_width - end_width
 goal_y_coord = board_curve_y + sqrt(board_radius^2 - (board_radius-end_width)^2)
 ### coordinates of the faceoff dots
-dzone_dot_x = rink_width/2 - end_width - dot_displacement
-dot_y = dot_span/2
+dzone_dot_x = rink_width - end_width - dot_displacement
 ### coordinates of the goal crease (end of straight line & angle where circle is truncated)
-crease_x_coord = -rink_width/2 + end_width + sqrt(crease_radius^2-(crease_width/2)^2)
-crease_theta = asin(crease_width/2/crease_radius)
+crease_x_coord = rink_width - end_width - sqrt(crease_radius^2-crease_width^2)
+crease_theta = asin(crease_width/crease_radius)
 ## colors
 nhl_red = '#cf142b'  # PMS 186
 nhl_blue = '#0033ab' # PMS 286
-# a vertical segment symmetric about 0
-plot_symm_vseg = function(x, y, ...) lines(c(x, x), c(-y, y), ...)
-# a horizontal segment symmetric about 0
-plot_symm_hseg = function(x, y, ...) lines(c(-x, x), c(y, y), ...)
+# a vertical segment starting at 0
+plot_symm_vseg = function(x, y, ...) lines(c(x, x), c(0, y), ...)
+# a horizontal segment starting at 0
+plot_symm_hseg = function(x, y, ...) lines(c(0, x), c(y, y), ...)
+# simplest way I could come up with to go from multipliers -> quadrants,
+#   to use to get the arc endpoints for circular segments (e.g.
+#   the end boards go from 0 to pi/2 in quadrant 0, pi/2 to pi in quadrant 1, ...
+#   there may be something with asin & acos that can be applied to +/-1 but it
+#   was tantalizingly out of reach.
+get_quadrant = function(x) {
+  switch(paste(x, collapse=''), '11' = 0, '-11' = 1, '-1-1' = 2, '1-1' = 3)
+}
 # a circle
 plot_circle = function(h, k, r, ...) {
   tgrid = seq(0, 2*pi, length.out = 100L)
@@ -101,12 +99,15 @@ plot_circle = function(h, k, r, ...) {
 # a filled circle
 plot_filled_circle = function(h, k, r, ...) {
   tgrid = seq(0, 2*pi, length.out = 100L)
-  polygon(h + r*cos(tgrid), k + r*sin(tgrid), border = NA, ...)
+  polygon(h + r*cos(tgrid), k + r*sin(tgrid), border=NA, ...)
 }
 # a circular arc between angles (in radians) t0 and t1
 plot_circle_arc = function(h, k, r, t0, t1, ...) {
   tgrid = seq(t0, t1, length.out = 100L)
-  xseq = h + r*cos(tgrid)
-  yseq = k + r*sin(tgrid)
-  lines(xseq, yseq, ...)
+  lines(h + r*cos(tgrid), k + r*sin(tgrid), ...)
+}
+# a filled arc between angles (in radians) t0 and t1
+plot_sector = function(h, k, r, t0, t1, ...) {
+  tgrid = seq(t0, t1, length.out = 100L)
+  polygon(h + c(0, r*cos(tgrid), 0), k + c(0, r*sin(tgrid), 0), border=NA, ...)
 }
