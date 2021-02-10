cd81441b9d0c651932a36d485dad6992e74d0f4d
Jozef Hajnala <jozef.hajnala@gmail.com>
2021-02-10 20:01:00 +0100

    Add test for plot_rink()

---
+++ tests/testthat/test.plot_rink.R
@@ -0,0 +1,20 @@
+context("plot_rink")
+
+expect_image_equal <- function(object, expected, ...) {
+  # compare svg character vs reference
+  # helpers are defined in setup.R
+  stopifnot(is.character(expected) && file.exists(expected))
+  testthat::expect_equal(
+    round_svg_numbers(ignore_svg_id(plot_image(object))),
+    round_svg_numbers(ignore_svg_id(readLines(expected))),
+    ...
+  )
+}
+
+testthat::test_that(
+  "Rink generated with plot_rink() is equal to prototype",
+  expect_image_equal(
+    plot_rink(),
+    "prototype/rink.svg"
+  )
+)
